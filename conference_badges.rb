6a498aa5aafc026cc44c75ac3d5389b73515b4fd
michael hohl <mhohl@rocketmail.com>
2018-03-19 19:44:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,3 +7,9 @@ end
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def assign_rooms(speakers)
+  room_assignments = []
+  speakers.each_with_index {|speaker, i| room_assignments << "Hello, #{speaker}! You'll be assigned to room #{i + 1}!"}
+  room_assignments
+end
